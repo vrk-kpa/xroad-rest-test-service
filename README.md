5509368da503475cae12f04766b28b5fea3a5656
Mikko Seppälä <mikko.seppala@gofore.com>
2023-12-05 10:30:33 +0200

    Add context path '/rest-api' for the app

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ docker-compose up
 
 Then you can test calling the rest-test-service directly, without the security server:
 ```shell
-curl -X GET localhost:8070/random
+curl -X GET localhost:8070/rest-api/random
 ```
 
 It takes a while for the security server to start up. You can check the logs with `docker compose logs -f ss`.
@@ -31,7 +31,7 @@ Standalone security server has one X-Road service and one client,
 named `CS:ORG:1111:TestService` and `CS:ORG:1111:TestClient` respectively.
 Add the rest-test-service to the service list of `CS:ORG:1111:TestService` as a REST service.
 Service base URL in docker network is `http://test-service:8080`. When adding the service you can provide the service's
-OpenApi description URL: `http://test-service:8080/v3/api-docs`. Security server will then automatically add all 
+OpenApi description URL: `http://test-service:8080/rest-api/api-docs`. Security server will then automatically add all 
 the service endpoints when creating the X-Road service. Set the service-code to `rest-test` and remember to enable the 
 service.
 
@@ -45,5 +45,5 @@ After the config you can call the rest-test-service through the security server:
 curl \
   -X GET \
   -H 'X-Road-Client: CS/ORG/1111/TestClient' \
-  -i 'http://localhost:8080/r1/CS/ORG/1111/TestService/rest-test/random'
+  -i 'http://localhost:8080/r1/CS/ORG/1111/TestService/rest-test/rest-api/random'
 ```
--- src/test/java/fi/dvv/xroad/resttestservice/XroadRestTestServiceApplicationTests.java
+++ src/test/java/fi/dvv/xroad/resttestservice/XroadRestTestServiceApplicationTests.java
@@ -5,9 +5,11 @@ import fi.dvv.xroad.resttestservice.model.GreetingDto;
 import fi.dvv.xroad.resttestservice.model.RandomNumberDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 
 import java.net.URI;
@@ -20,52 +22,59 @@ class ApiTest {
     @LocalServerPort
     private int port;
 
+    @Value("${server.servlet.contextPath}")
+    private String contextPath;
+
     @Autowired
     private TestRestTemplate restTemplate;
 
+    private String baseUrl() {
+        return "http://localhost:" + port + contextPath;
+    };
+    
     @Test
     void randomReturnsRandomNumber() throws Exception {
-        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/random", RandomNumberDto.class).randomNumber()).isGreaterThanOrEqualTo(1).isLessThanOrEqualTo(100);
+        assertThat(this.restTemplate.getForObject(baseUrl() + "/random", RandomNumberDto.class).randomNumber()).isGreaterThanOrEqualTo(1).isLessThanOrEqualTo(100);
     }
 
     @Test
     void greetingReturnsGreetingMessage() throws Exception {
-        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/greeting", GreetingDto.class).greeting()).isEqualTo("Hello! Greetings from adapter server!");
+        assertThat(this.restTemplate.getForObject(baseUrl() + "/greeting", GreetingDto.class).greeting()).isEqualTo("Hello! Greetings from adapter server!");
     }
 
     @Test
     void greetingReturnsGreetingMessageWithName() throws Exception {
-        assertThat(this.restTemplate.getForObject(new URI("http://localhost:" + port + "/greeting?name=Gandalf"), GreetingDto.class).greeting()).isEqualTo("Hello Gandalf! Greetings from adapter server!");
+        assertThat(this.restTemplate.getForObject(new URI(baseUrl() + "/greeting?name=Gandalf"), GreetingDto.class).greeting()).isEqualTo("Hello Gandalf! Greetings from adapter server!");
     }
 
     @Test
     void greetingReturnsGreetingMessageWithComplexName() throws Exception {
-        assertThat(this.restTemplate.getForObject(new URI("http://localhost:" + port + "/greeting?name=X%20%C3%86%20A-12"), GreetingDto.class).greeting()).isEqualTo("Hello X Æ A-12! Greetings from adapter server!");
+        assertThat(this.restTemplate.getForObject(new URI(baseUrl() + "/greeting?name=X%20%C3%86%20A-12"), GreetingDto.class).greeting()).isEqualTo("Hello X Æ A-12! Greetings from adapter server!");
     }
 
     @Test
     void greetingReturnsGreetingMessageWithNameEscapedForJson() throws Exception {
-        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/greeting?name=<script>alert('Executed!');</script>", GreetingDto.class).greeting()).isEqualTo("Hello <script>alert('Executed!');<\\/script>! Greetings from adapter server!");
+        assertThat(this.restTemplate.getForObject(baseUrl() + "/greeting?name=<script>alert('Executed!');</script>", GreetingDto.class).greeting()).isEqualTo("Hello <script>alert('Executed!');<\\/script>! Greetings from adapter server!");
     }
 
     @Test
     void greetingReturnsErrorForTooLongName() throws Exception {
         String name = "a".repeat(257);
-        ErrorDto error = this.restTemplate.getForObject("http://localhost:" + port + "/greeting?name=" + name, ErrorDto.class);
+        ErrorDto error = this.restTemplate.getForObject(baseUrl() + "/greeting?name=" + name, ErrorDto.class);
         assertThat(error.errorMessage()).isEqualTo("Name is too long. Max length is 256 characters.");
         assertThat(error.httpStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
     @Test
     void nonExistentEndpointReturnsError() throws Exception {
-        ErrorDto error = this.restTemplate.getForObject("http://localhost:" + port + "/i-dont-exist", ErrorDto.class);
-        assertThat(error.errorMessage()).isEqualTo("No endpoint GET /i-dont-exist.");
+        ErrorDto error = this.restTemplate.getForObject(baseUrl() + "/i-dont-exist", ErrorDto.class);
+        assertThat(error.errorMessage()).isEqualTo("No endpoint GET /rest-api/i-dont-exist.");
         assertThat(error.httpStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
     }
 
     @Test
     void wrongMethodReturnsError() throws Exception {
-        ErrorDto error = this.restTemplate.postForObject("http://localhost:" + port + "/random", "foo", ErrorDto.class);
+        ErrorDto error = this.restTemplate.postForObject(baseUrl() + "/random", "foo", ErrorDto.class);
         assertThat(error.errorMessage()).isEqualTo("Request method 'POST' is not supported");
         assertThat(error.httpStatus()).isEqualTo(HttpStatus.METHOD_NOT_ALLOWED.value());
     }
